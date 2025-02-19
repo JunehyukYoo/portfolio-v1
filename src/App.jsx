dd0a0f5be09457cf47f72600bf43c27090909a2d
June <yoo.junehyuk@gmail.com>
2025-02-19 11:00:38 -0600

    Completed loading screen, now on navbar.
    Implemented animations and the loading screen fading away to the
    home page once finished. Currently I have added a basic logo and some
    basic links that do not work yet.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react';
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css"
+import { Navbar } from './components/Navbar';
 
 function App() {
   // eslint-disable-next-line no-unused-vars
   const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <>{<LoadingScreen onComplete = {() => setIsLoaded(true)}/>}</>
+    <>{!isLoaded && <LoadingScreen onComplete = {() => setIsLoaded(true)}/>}{" "}
+    <div 
+      className={`min-h-screen transition-opacity duration-700 
+      ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}
+    >
+        <Navbar/>
+    </div>
+
+    </>
   );
 }
 
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 // eslint-disable-next-line react/prop-types
 export const LoadingScreen = ({ onComplete }) => {
     const [text, setText] = useState("");
-    const fullText = "Hello world";
+    const fullText = "console.log(\"hello world\")";
 
     useEffect(() => {
         let idx = 0;
@@ -13,10 +13,12 @@ export const LoadingScreen = ({ onComplete }) => {
 
             if (idx > fullText.length) {
                 clearInterval(interval);
+
                 setTimeout(() => {
                     onComplete();
                 }, 1000);
             }
+            
         }, 100);
 
         return () => clearInterval(interval);
@@ -25,7 +27,7 @@ export const LoadingScreen = ({ onComplete }) => {
     return <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
         <div className="mb-4 text-4xl font-mono font-bold"> {text} <span className="animate-blink ml-1"> | </span></div>
 
-        <div className="w-[200px] h-[200px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
             <div className="w-[40%] h-full bg-blue-500 shadow-[]0_0_15px_#3b82f6] animate-loading-bar">{" "}</div>
         </div>
     </div>
