fe66d239cac69f70235dd53a44f519c8c2f92945
June <yoo.junehyuk@gmail.com>
2025-02-19 01:39:09 -0600

    Created necessary component files.
    Began implementation of laoding screen.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './App.css'
+import { LoadingScreen } from './components/LoadingScreen';
+import "./index.css"
 
 function App() {
-  return <></>;
+  // eslint-disable-next-line no-unused-vars
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <>{<LoadingScreen onComplete = {() => setIsLoaded(true)}/>}</>
+  );
 }
 
 export default App
